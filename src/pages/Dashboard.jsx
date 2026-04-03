e92955ab2fd68debcf1344d64366d45b36431291
Arun Prakash <m.arunprakashnkl@gmail.com>
2026-04-03 20:22:56 +0530

    Hide

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -194,7 +194,7 @@ export default function Dashboard({ user, isDark }) {
     <>
       <div className="page-header">
         {/* <div className="page-title">Welcome back, {user.name}</div> */}
-        <div className="page-subtitle">Here is your customized {user.name} dashboard overview.</div>
+        {/* <div className="page-subtitle">Here is your customized {user.name} dashboard overview.</div> */}
       </div>
       
       {user.name === 'Admin' && <AdminDashboard />}
